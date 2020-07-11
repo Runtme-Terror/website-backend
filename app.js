297538acccc6b2ee7591b0f9c27443170367396c
Soumik Dhua <soumik.dhua@gmail.com>
2020-07-11 21:22:18 +0530

    routes/

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const MongoStore = require('connect-mongo')(session);
 const Doctor = require('./model/doctor');
 const passport = require('passport');
 const path = require('path');
+const doctorRoute = require('./routes/doctor');
 
 dotenv.config({path: './.env'});
 
@@ -15,6 +16,8 @@ const publicDirectory = path.join(__dirname, './public');         // Join path t
 app.use(express.static(publicDirectory));                         // Serve public files like CSS,Favicon
 app.use(express.urlencoded({extended: false}));                   // For parsing URL encoded bodies
 app.use(express.json());
+
+
 
 app.set('view engine', 'ejs');
 
@@ -41,71 +44,19 @@ app.use(session({
 app.use(passport.initialize());             // Initialize Passport 
 app.use(passport.session());
 
+
+app.use('/doctor', doctorRoute);
+
 /////////////////////////////////////////////////////////////////////////////
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
-app.get('/doctor', (req, res) => {
-    res.render('doctor');
-});
-
-app.get('/doctor/home', (req, res) => {
-    res.render('doctorhome');
-});
-
-app.post('/doctor/register', async(req, res) => {
-    const registration_id = req.body.registration_id;
-    const name = req.body.name;
-    const phone_number = req.body.phone_number;
-    const email = req.body.email;
-    const password = req.body.password;    
-
-    try {
-        const newDoctor = {
-            registration_id: registration_id,
-            name: name,
-            phone_number: phone_number,
-            email: email,
-            password: password
-            
-        };
-        doctor = await Doctor.create(newDoctor);            
-        res.redirect('/doctor');
-
-    } catch (error) {
-        console.log(error);
-        res.redirect('/doctor');
-    }
-});
-
-app.post('/doctor/login', async(req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;    
-
-    const query = { email: email, password: password};
-
-    try {
-        let doctor = await Doctor.findOne(query);
-        console.log(doctor);
-
-        if(doctor){
-            req.login(email, () =>{
-                res.redirect('/doctor/home')
-            });
-        } else {
-            res.redirect('/doctor');
-        }
 
 
-    } catch (error) {
-        console.log(error);
-        res.redirect('/doctor');
-    }
 
 
-});
 
 
 app.get('/logout', (req, res) => {
@@ -115,11 +66,3 @@ app.get('/logout', (req, res) => {
 });
 
 
-// Passport Serialize and Deserialize
-passport.serializeUser(function(email, done) {
-    done(null, email);
-  });
-  
-passport.deserializeUser(function(email, done) {
-    done(null, email);
-  });
---
+++ routes/doctor.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const passport = require('passport');
+const path = require('path');
+const Doctor = require('../model/doctor');
+
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.render('doctor');
+});
+
+router.post('/register', async(req, res) => {
+    const registration_id = req.body.registration_id;
+    const name = req.body.name;
+    const phone_number = req.body.phone_number;
+    const email = req.body.email;
+    const password = req.body.password;    
+
+    try {
+        const newDoctor = {
+            registration_id: registration_id,
+            name: name,
+            phone_number: phone_number,
+            email: email,
+            password: password
+            
+        };
+        doctor = await Doctor.create(newDoctor);            
+        res.redirect('/doctor');
+
+    } catch (error) {
+        console.log(error);
+        res.redirect('/doctor');
+    }
+});
+
+router.post('/login', async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;    
+
+    const query = { email: email, password: password};
+
+    try {
+        let doctor = await Doctor.findOne(query);
+        console.log(doctor);
+
+        if(doctor){
+            req.login(doctor, () =>{
+                res.redirect('/doctor/home')
+            });
+        } else {
+            res.redirect('/doctor');
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.redirect('/doctor');
+    }
+
+
+});
+
+
+router.get('/home', authenticationMiddleware(), (req, res) => {
+    res.render('doctorhome');
+});
+
+
+
+
+
+
+
+function authenticationMiddleware () {  
+	return (req, res, next) => {
+        if (req.isAuthenticated()) return next();        
+	    res.redirect('/doctor');
+	}
+}
+
+
+// Passport Serialize and Deserialize
+passport.serializeUser(function(doctor, done) {
+    done(null, doctor.id);
+  });
+  
+passport.deserializeUser( (id, done) => {
+Doctor.findById(id, (err, doctor) =>  done(null, doctor));
+});
+
+module.exports = router;
